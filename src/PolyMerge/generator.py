a9e8d1acf2614fbbb9dc4dbb7df9771e25fa4f8c
Daniel Stokes <>
2019-03-11 19:57:59 +1300

    Made generator more random

--- src/PolyMerge/generator.py
+++ src/PolyMerge/generator.py
@@ -4,7 +4,7 @@ num_runs = 10000
 for i in range(0, num_runs):
     string = ""
     for j in range(0, random.randint(1, 1000)):
-        string += str(j)
-        print(string + " " +str(i))
+        string += chr(random.randint(1, 27) + 64)
+        print(string)
 
     print()
