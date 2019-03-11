a3322dad4f593a06e0393ab44226d66dbfa838bd
Daniel Stokes <>
2019-03-11 20:21:19 +1300

    Made better random number generator

--- src/PolyMerge/generator.py
+++ src/PolyMerge/generator.py
@@ -1,10 +1,16 @@
 import random
+import string
 
 num_runs = 10000
 for i in range(0, num_runs):
-    string = ""
-    for j in range(0, random.randint(1, 1000)):
-        string += chr(random.randint(1, 27) + 64)
-        print(string)
+    run = []
+    for j in range(0, random.randint(100, 500)):
+        record = ""
+        for k in range(0, 16):
+            record += random.choice(string.ascii_letters)
+        run.append(record)
 
+    run.sort()
+    for line in run:
+        print(line)
     print()
