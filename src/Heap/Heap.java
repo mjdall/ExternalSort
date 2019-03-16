5ca1e39a6f43e809c5d19d3735a37f775cb569ae
Daniel Stokes <>
2019-03-16 20:26:11 +1300

    Commented and cleaned up

--- src/Heap/Heap.java
+++ src/Heap/Heap.java
@@ -1,5 +1,8 @@
 package Heap;
 
+// Make IntelliJ happy
+import Heap.Comparer;
+
 // TODO: Do I need to handle error conditions?
 
 public class Heap <T extends Comparable<T>> {
@@ -8,154 +11,260 @@ public class Heap <T extends Comparable<T>> {
     private Comparer comparer;
 
     public Heap (T[] rawArray, Comparer comparisonFunction) {
+        // Set members
         this.comparer = comparisonFunction;
         this.heap = rawArray;
         this.heapSize = rawArray.length;
-        buildHeap();
+
+        // Put the array in heap order
+        resetHeap();
     }
 
+    /**
+     * Gets the index of nodes parent
+     * @param index Index of the child
+     * @return Index of parent
+     */
     private int getParentIndex (int index) { return (index - 1) / 2; }
 
+    /**
+     * Gets the index of left child
+     * @param index Parent index
+     * @return Index of left child
+     */
     private int getLeftChildIndex (int index) { return index * 2 + 1; }
 
+    /**
+     * Checks index is in range
+     * @param index Index to check
+     * @return True if in range, false otherwise
+     */
     private boolean checkIndex (int index) { return index < heapSize; }
 
+    /**
+     * Gets size of the heap
+     * @return Size of the heap
+     */
     public int getHeapSize () { return heapSize; }
 
+    /**
+     * Gets the max possible capacity of the heap
+     * @return The max capacity of the heap
+     */
     public int getMaxHeapSize () { return heap.length; }
 
+    /**
+     * Gets the number of free slots in the heap
+     * @return Nummber of free slots in the heap
+     */
     public int getFreeNodes () { return heap.length - heapSize; }
 
+    /**
+     * Gets the underlying array for the heap
+     * @return The underlying array
+     */
     public T[] getHeap () { return heap; }
 
+    /**
+     * Heapifies array not in heap order
+     */
     private void buildHeap () {
+        // Get first node that has a child
         int startFrom = getParentIndex(heapSize - 1);
+        // Traverse all nodes and put them in heap order
+        // Start from last parent node so that higher nodes have sub heaps as children
         for (int i = startFrom; i >= 0; i--) {
-            int insIndex = i;
-            while (insIndex >= 0) { insIndex = trySwapDown(insIndex); }
+            downHeap(i);
         }
     }
-//
-//    private void heapify (int fromIndex) {
-//        T curr = heap[fromIndex];
-//        int lIdx = getLeftChildIndex(fromIndex);
-//        int rIdx = lIdx + 1;
-//
-//        if (!checkIndex(lIdx)) { return; }
-//
-//        T leftChild = heap[lIdx];
-//        int leftComparison = comparer.compare(curr, leftChild);
-//        int largest = leftComparison;
-//
-//        if (checkIndex(rIdx)) {
-//            T rightChild = heap[rIdx];
-//            largest = Math.max(leftComparison, comparer.compare(curr, rightChild));
-//        }
-//
-//        if (largest <= 0) { return; }
-//
-//        int modifiedIndex = largest == leftComparison ? lIdx : rIdx;
-//        heap[fromIndex] = heap[modifiedIndex];
-//        heap[modifiedIndex] = curr;
-//        heapify(modifiedIndex);
-//    }
 
+    /**
+     * Swaps first and last element in the heap
+     */
     private void swapHeadAndTail () {
-        if (heapSize <= 0) { return; }
+        // Check that the heap has something to swap
+        if (heapSize <= 1) { return; }
+        // Swap head and tail
         T head = heap[0];
         heap[0] = heap[heapSize - 1];
         heap[heapSize - 1] = head;
     }
 
+    /**
+     * Perform upheap operation
+     * @param index Index to start at
+     */
+    private void upHeap( int index ) {
+        // Swap up until we find the correct place
+        while (index > 0) { index = trySwapUp(index); }
+    }
+
+    /**
+     * Perform down heap operation
+     * @param index Index to start at
+     */
+    private void downHeap( int index ) {
+        // Swap down until we find the correct place
+        while (index >= 0) { index = trySwapDown(index); }
+    }
+
+    /**
+     * Swap node with parent if smaller
+     * @param nodeIndex Node to try to swap
+     * @return New index of the node or -1 if not swapped
+     */
     private int trySwapUp (int nodeIndex) {
+        // Check we have a parent to swap with
         if (nodeIndex <= 0) { return -1; }
 
+        // Get parent value
         int parentIndex = getParentIndex(nodeIndex);
         T currentItem = heap[nodeIndex];
         T parentItem = heap[parentIndex];
 
-        // if the currentItem has a higher priority than it's parent
+        // Check if we need to swap
         if (comparer.compare(parentItem, currentItem) >= 0) { return -1; }
 
+        // Swap with parent
         heap[parentIndex] = currentItem;
         heap[nodeIndex] = parentItem;
         return parentIndex;
     }
 
+    /**
+     * Swap node with child if larger
+     * @param nodeIndex Node to try to swap
+     * @return New index of the node or -1 if not swapped
+     */
     private int trySwapDown (int nodeIndex) {
-        // if it's at the bottom or can't be moved
+        // If it's at the bottom
         if (nodeIndex == heapSize - 1 || nodeIndex < 0) { return -1; }
+
+        // Get child indexes
         int lIdx = getLeftChildIndex(nodeIndex);
         int rIdx = lIdx + 1;
 
-        // no children
+        // Left child is out of range - No children
         if (!checkIndex(lIdx)) { return -1; }
 
+        // Get left child value
         T currNode = heap[nodeIndex];
         T leftChild = heap[lIdx];
         T currCompareNode = leftChild;
         int comparisonIndex = lIdx;
+
+        // Check if we have right child
         if (checkIndex(rIdx)) {
             T rightChild = heap[rIdx];
+            // Check if right child is less than left
             if (comparer.compare(leftChild, rightChild) > 0) {
                 currCompareNode = rightChild;
                 comparisonIndex = rIdx;
             }
         }
-        int parentCompare = comparer.compare(currNode, currCompareNode);
 
-        // if smallest < 0 then the current item has less priority
+        // Check if we need to swap - <= 0 means parent is smaller, dont swap down
+        int parentCompare = comparer.compare(currNode, currCompareNode);
         if (parentCompare <= 0) { return -1; }
 
+        // Swap down
         heap[nodeIndex] = heap[comparisonIndex];
         heap[comparisonIndex] = currNode;
+        // Return new index
         return comparisonIndex;
     }
 
+    /**
+     * Insert element into the heap
+     * @param newItem Item to insert
+     * @return True if the heap had space for the item
+     */
     public boolean insert (T newItem) {
+        // Check if we have space
         if (heapSize == heap.length) { return false; }
+        // Insert new node
         int insIndex = heapSize;
         heapSize++;
         heap[insIndex] = newItem;
 
-        while (insIndex > 0) { insIndex = trySwapUp(insIndex); }
+        // Perform upheap operation
+        upHeap(insIndex);
         return true;
     }
 
-    public T remove () {
+    /**
+     * Remove top element from the heap
+     * @param keep If the element should be kept at the end of the array
+     * @return The removed element, null if heap is empty
+     */
+    public T remove (boolean keep) {
+        // Check there is something to remove
         if (heapSize == 0) { return null; }
+
+        // Swap with last spot in heap
         swapHeadAndTail();
+        // Decrease heap size
         heapSize--;
-        int insIndex = 0;
-        while (insIndex >= 0) { insIndex = trySwapDown(insIndex); }
-        return heap[heapSize];
+        // Down heap swapped element to restore heap order
+        downHeap(0);
+
+        T removed = heap[heapSize];
+        // Set old value to null if not keeping it
+        if(!keep) {
+            heap[heapSize] = null;
+        }
+
+        return removed;
     }
 
+    /**
+     * Remove root element from heap and insert new item. Does not insert new item if heap is empty
+     * @param newItem New item to insert
+     * @return Removed element, null if heap is empty
+     */
     public T replace (T newItem) {
+        // Check there is something to replace
         if (heapSize == 0) { return null; }
-        int insertAt = 0;
-        T curr = heap[insertAt];
-        heap[insertAt] = newItem;
-        while (insertAt >= 0) { insertAt = trySwapDown(insertAt); }
+        // Replace element
+        T curr = heap[0];
+        heap[0] = newItem;
+        // Down heap new element to restore heap order
+        downHeap(0);
         return curr;
     }
 
-    public T check () {
+    /**
+     * Peek at the element on the top of the heap
+     * @return The element on top of the heap
+     */
+    public T peek () {
+        // Check there is element to peek at
         if (heapSize == 0) { return null; }
+        // Return element
         return heap[0];
     }
 
+    /**
+     * Restores unordered heap array to heap ordering
+     * @return The size of the new heap
+     */
     public int resetHeap () {
-        // get the first occurence of an item, this ideally should be the max size
-        int heapSize = heap.length;
+        // Compact all values, skipping nulls
+        int shift = 0;
+        for(int i = 0; i < heap.length; i++) {
+            if(heap[i] == null) {
+                shift++;
+            } else {
+                heap[i-shift] = heap[i];
+            }
+        }
+        // Calculate new heap size
+        this.heapSize = heap.length - shift;
 
-        while (heap[heapSize - 1] == null) { heapSize--; }
-        this.heapSize = heapSize;
-
-        // rebuild the heap
+        // Rebuild the heap
         buildHeap();
 
-        // return the number of free slots in the heap
-        return heap.length - heapSize;
+        // Return the size of the heap
+        return getHeapSize();
     }
 }
--- src/MakeRuns/MakeRuns.java
+++ src/MakeRuns/MakeRuns.java
@@ -3,42 +3,70 @@ package MakeRuns;
 import Heap.Comparers.StringComparer;
 import Heap.Heap;
 import Heap.Comparer;
-import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Arrays;
 
 public class MakeRuns {
     final static private String usage = "Usage: java MakeRuns <memory_size> <input_file> <output_file>";
 
+    /**
+     * Get usage string
+     * @return Usage string
+     */
     private static String getUsage () { return usage; }
 
+    /**
+     * Print error and exit
+     * @param toPrint Error message to print
+     */
     private static void printAndExit (String toPrint) {
         System.out.println(toPrint);
         System.exit(1);
     }
 
-    private static int getRunSize (String arg) {
+    /**
+     * Get the size of the heap to use from the command line argument
+     * @param arg The argument to parse
+     * @return The parsed value
+     */
+    private static int getHeapSize(String arg) {
         int memorySize = 0;
         try { memorySize = Integer.parseInt(arg); }
         catch (NumberFormatException e) {
-            printAndExit(String.format("could not parse `memory_size` argument `%s`", arg));
+            printAndExit(String.format("Could not parse `memory_size` argument `%s`", arg));
         }
-        if (memorySize <= 0) { printAndExit("memory size has to be greater than zero"); }
+        if (memorySize <= 0) { printAndExit("Memory size has to be greater than zero"); }
         return  memorySize;
     }
 
+    /**
+     * Open and clear the contents of the file
+     * @param file File to open
+     * @return BufferedWriter to the file
+     * @throws IOException
+     */
     private static BufferedWriter openAndClearFile (File file) throws IOException {
         return Files.newBufferedWriter(Paths.get(file.toURI()), StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
     }
 
+    /**
+     * Opens file for reading
+     * @param file The file to open for reading
+     * @return BufferedReader for the file
+     * @throws IOException
+     */
     private static BufferedReader openReadFile(File file) throws IOException {
         return Files.newBufferedReader(Paths.get(file.toURI()));
     }
 
+    /**
+     * Get output stream for specified file
+     * @param outputFilename Path to the file
+     * @return BufferedWriter for the file
+     */
     private static BufferedWriter getOutStream (String outputFilename) {
         try {
             File outFile = new File(outputFilename);
@@ -50,6 +78,11 @@ public class MakeRuns {
         }
     }
 
+    /**
+     * Get input stream for specified file
+     * @param inputFilename Path to the file
+     * @return BufferedReader for the file
+     */
     private static BufferedReader getInStream (String inputFilename) {
         try {
             File inFile = new File(inputFilename);
@@ -61,23 +94,39 @@ public class MakeRuns {
         }
     }
 
+    /**
+     * Read line from input
+     * @param input BufferedReader to read with
+     * @return Line, or null if input is finished
+     */
     private static String tryReadLine (BufferedReader input) {
         try {
             return input.readLine();
         } catch (java.io.IOException e) {
-            printAndExit("rip");
+            printAndExit("Error reading from file\n\n" + e.getMessage());
             return null;
         }
     }
 
+    /**
+     * Write line to output
+     * @param toWrite Line to write
+     * @param output BufferedWriter to write with
+     */
     private static void tryWriteLine (String toWrite, BufferedWriter output) {
         try {
             output.write(toWrite + "\n");
         } catch (java.io.IOException e) {
-            printAndExit("rip");
+            printAndExit("Error writing to file\n\n" + e.getMessage());
         }
     }
 
+    /**
+     * Read in the initial runs into the heap
+     * @param runSize The size of the heap
+     * @param input The input stream to read from
+     * @return Array containing the elements
+     */
     private static String[] readInInitial (int runSize, BufferedReader input) {
         String[] finalArray = new String[runSize];
         for (int i = 0; i < runSize; i++) {
@@ -86,44 +135,50 @@ public class MakeRuns {
         return finalArray;
     }
 
-    private static void writeFinalQueue (Heap<String> priorityQueue, BufferedWriter oStream) {
-        tryWriteLine("", oStream);
-        priorityQueue.resetHeap();
-        String popped;
-        while ((popped = priorityQueue.remove()) != null) {
-            tryWriteLine(popped, oStream);
-        }
-        tryWriteLine("", oStream);
-        System.out.println("Complete");
-        System.exit(0);
-    }
-
+    /**
+     * Runs the replacement selection strategy algorithm to generate runs
+     * @param runSize Size of the heap to use
+     * @param iStream BufferedReader to get input from
+     * @param oStream BufferedWriter to write runs to
+     */
     private static void runMakeRuns (int runSize, BufferedReader iStream, BufferedWriter oStream) {
         try {
+            // Read in initial values and create heap
             Comparer comparer = new StringComparer();
             String[] initialHeapArray = readInInitial(runSize, iStream);
             Heap<String> priorityQueue = new Heap<String>(initialHeapArray, comparer);
 
             String lastOut = null;
+            // Loop while there are values to process
             while (iStream.ready() || priorityQueue.getHeapSize() > 0) {
-                String top = priorityQueue.check();
+                // Get next value
+                String top = priorityQueue.peek();
+                // Check if value can be written to output stream
                 if(lastOut == null || comparer.compare(top, lastOut) >= 0) {
+                    // Write value out
                     lastOut = top;
                     tryWriteLine(top, oStream);
+                    // Replace written element with next
                     if(iStream.ready())
                         priorityQueue.replace(tryReadLine(iStream));
+                    // No next element - remove from the heap permanantly
                     else
-                        priorityQueue.remove();
+                        priorityQueue.remove(false);
                 } else {
-                    priorityQueue.remove();
-                    if(priorityQueue.getHeapSize() == 0 && iStream.ready()) {
+                    // Remove from the heap, saving the value at the end of the array
+                    priorityQueue.remove(true);
+                    // Check if we have processed all the elements
+                    if(priorityQueue.getHeapSize() == 0) {
+                        // Start next run
                         lastOut = null;
                         tryWriteLine("", oStream);
+                        // Reset the heap
                         priorityQueue.resetHeap();
                     }
                 }
             }
 
+            // Close files
             oStream.flush();
             iStream.close();
             oStream.close();
@@ -133,21 +188,18 @@ public class MakeRuns {
         }
     }
 
-    private static void popAll (Heap<String> heap) {
-        String curr;
-        while ((curr = heap.remove()) != null) {
-            System.out.println(curr);
-        }
-        System.exit(1);
-    }
-
     public static void main (String[] args) {
+        // Check args
         if (args.length != 3) { printAndExit(getUsage()); }
-        int runSize = getRunSize(args[0]);
+        // Get size of heap to use for runs
+        int runSize = getHeapSize(args[0]);
+        // Get input/output files
         String inputFilename = args[1];
         String outputFilename = "./" + args[2];
+        // Open files for reading and writing
         BufferedReader inStream = getInStream(inputFilename);
         BufferedWriter outStream = getOutStream(outputFilename);
+        // Run replacement selection algorithm to generate runs
         runMakeRuns(runSize, inStream, outStream);
     }
 }
