563cc0d63cfa71c496ddeb4f6ba55d1342a62a4f
Daniel Stokes <>
2019-03-28 21:46:47 +1300

    Better generator

--- src/PolyMerge/generator.py
+++ src/PolyMerge/generator.py
@@ -1,16 +1,16 @@
-import random
-import string
+rand_gen = 0xD5
+def next_rand():
+    global rand_gen
+    rand_gen = (rand_gen * 1103515245 + 12345) & 0xFFFFFFFF
+    return rand_gen
 
-num_runs = 10000
+num_runs = 100000000
 for i in range(0, num_runs):
     run = []
-    for j in range(0, random.randint(100, 500)):
-        record = ""
-        for k in range(0, 16):
-            record += random.choice(string.ascii_letters)
-        run.append(record)
-
-    run.sort()
-    for line in run:
-        print(line)
-    print()
+    if next_rand() & 0x7F == 0:
+        run.append('')
+        continue
+    record = ""
+    for k in range(0, 16):
+        record += chr(next_rand() % 26 + ord('A'))
+    print(record)
